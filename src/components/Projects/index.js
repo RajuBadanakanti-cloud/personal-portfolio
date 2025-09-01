fbc8830b7f031cf846e9c2447b53fdd49d6a2b5c
Raju Rj <rajubadanakanti166@gmail.com>
2025-09-01 21:23:43 +0530

    Polished

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -8,25 +8,25 @@ const userProjectsData = [{
     projectTitle:"Todo",
     description:"A React & CSS To-Do App to easily manage and prioritize daily tasks. Add, complete, delete, and save tasks with a clean, responsive interface",
     coverImage:"https://res.cloudinary.com/dnh9hnjbx/image/upload/v1756365784/Projects%20Resources/Todo-App-Project-cover-1:1.png",
-    projectDeploymentURL:"https://learn.unifiedmentor.com/products/home",
-    gitRepoURL:'https://github.com/RajuBadanakanti-cloud/My-Todo-App.git'
-
+    projectDeploymentURL:"https://rbcloud-mytodos-app.netlify.app/",
+    gitRepoURL:'https://github.com/RajuBadanakanti-cloud/my-todos-app.git'
 },
+
 {
     id:2,
     projectTitle:"Recipe Book",
     description:"A React-based Recipe Book to browse, add, delete and search recipes, ingredients, and preparation steps along with dish images. Includes a detailed recipe view for easy cooking guidance",
     coverImage:"https://res.cloudinary.com/dnh9hnjbx/image/upload/v1756479073/Projects%20Resources/Recipe-Book-Cover-image.png",
-    projectDeploymentURL:"https://chatgpt.com/",
-    gitRepoURL:'https://github.com/RajuBadanakanti-cloud/My-Todo-App.git'
+    projectDeploymentURL:"https://rbcloud-recipebook.netlify.app/",
+    gitRepoURL:'https://github.com/RajuBadanakanti-cloud/recipe-book-app.git'
 },
 {
     id:3,
     projectTitle:"Quiz",
     description:"A Web Development Quiz App with timed questions, instant feedback, and a point-based scoring system. Start, answer, and review your results with a smooth and interactive interface",
     coverImage:"https://res.cloudinary.com/dnh9hnjbx/image/upload/v1756478734/Projects%20Resources/Interactive-Quiz-Cover-Image.png",
-    projectDeploymentURL:"https://chatgpt.com/",
-    gitRepoURL:'https://github.com/RajuBadanakanti-cloud/My-Todo-App.git'
+    projectDeploymentURL:"https://rbcloud-interactive-quiz.netlify.app/",
+    gitRepoURL:'https://github.com/RajuBadanakanti-cloud/interactive-quiz.git'
 },
 
 {
@@ -47,7 +47,7 @@ const Projects = () => (
             {userProjectsData.map(eachProject => (
               
             <li key={eachProject.id} className='grid flex-col items-center rounded-lg shadow-lg shadow-amber-400 mr-5 md:mr-10 mb-5 cursor-pointer'
-             onClick={() => window.open(eachProject.projectDeploymentURL, "_blank")}>   
+            onClick={() => window.open(eachProject.projectDeploymentURL,"__blank")}>   
              {/* we have a link to the entire card using window.open */}
                 
                 <div className='project-card-container'>
@@ -65,9 +65,9 @@ const Projects = () => (
                         {/* Project Navigation container */}
                         <div className="flex flex-row justify-between items-center">
                         {/* Project GitRepo => */}
-                        <a href='https://github.com/RajuBadanakanti-cloud' 
+                        <a href={eachProject.gitRepoURL} 
                         target="__blank" className="bg-white text-center text-amber-500 text-sm md:text-base rounded-full
-                        p-3 hover:text-slate-800 mr-6">
+                        p-3 hover:text-slate-800 mr-6" onClick={(e) => e.stopPropagation()}>
                         <FaGithub /></a>
 
                          {/* Project live View => */}                        
