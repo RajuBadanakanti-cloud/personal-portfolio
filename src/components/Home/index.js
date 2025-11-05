bc86659201c9607d30251df1002a65dd9b9bb7dd
Raju Badanakanti <rajubadanakanti7@gmail.com>
2025-11-05 20:15:38 +0530

    Profile Update

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,6 @@
 import './index.css'
 // Profile Image Link 
-const profileImgLink = "https://res.cloudinary.com/dnh9hnjbx/image/upload/v1757773090/My%20Personals/4KProfile-3-3_IMG_PhotoGrid_fn4dgc.png"
+const profileImgLink = "/4KProfile-3-3_IMG_PhotoGrid.png"
 const Home = () =>  (
 
      <div id="home" className="bg-slate-900 min-h-screen flex flex-col justify-center items-center md:ml-72">
